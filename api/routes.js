6f726e0435911c7764ee44d09f823454c5d84a92
thomashaile <gthomashaile@gmail.com>
2020-05-22 21:01:27 +0200

    creating routes

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,15 @@
-const controllers = require('./controllers.js');
+const controllers = require('./controllers');
 const express = require('express');
+const path = require('path');
+const routes = express.Router();
 
-const router = express.Router();
-
-router.get('/', controllers.hello);
+routes.get('/', controllers.home);
 
 // write your routes
+routes.get('/courses', controllers.courses);
+routes.get('/courses/:id', controllers.singleCourse);
+routes.post('/courses', controllers.addCourse);
+routes.put('/courses/:id', controllers.updateCourse);
+routes.delete('/courses/:id', controllers.deleteCourse);
 
-
-module.exports = router;
+module.exports = routes;
